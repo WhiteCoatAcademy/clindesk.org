29ca6a06e8295f68354d5f06148254031b2cfb6e
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-08 16:08:51 -0500

    Try #2.

--- clindesk.py
+++ clindesk.py
@@ -16,7 +16,7 @@ if not app.debug:
 
 @app.route("/")
 def hello():
-    return "Welcome to ClinDesk, staging. Now auto-deployed!"
+    return "Welcome to ClinDesk, staging. Now auto-deployed v2!"
 
 # Random-ish URL triggers a git pull for the staging deployment, only.
 @app.route("/github-pull-on-commit-M9tmMWz4XI")
