3ca338ba6f72d36349ebe6095d6e2aaf7228746a
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-08 17:51:28 -0500

    Move static files, setup static w/ CloudFront distribution.

--- clindesk.py
+++ clindesk.py
@@ -1,8 +1,12 @@
+import os
 import urlparse
 from flask import Flask, redirect, render_template, url_for, request
-app = Flask(import_name=__name__)
+app = Flask(import_name=__name__, static_folder='s')
 
-app.config['STATIC_ROOT'] = 'http://static.clindesk.org/'
+if os.environ.get('SUPERVISOR_PROCESS_NAME', False) == 'clindesk-staging':
+    app.config['STAGING'] = True
+else:
+    app.config['STATIC_ROOT'] = 'http://static.clindesk.org/s/'
 
 # Set up logging in prod.
 # TODO: Check this actually works w/ ec2 mail & firewall, etc.
@@ -41,8 +45,7 @@ def welcome():
 @app.route("/github-pull-on-commit-M9tmMWz4XI", methods=['POST'])
 def github_pull_on_commit():
     if request.environ['HTTP_X_REAL_IP'] in ('207.97.227.253', '50.57.128.197', '108.171.174.178'):
-        import os
-        if os.environ['SUPERVISOR_PROCESS_NAME'] == 'clindesk-staging':
+        if app.config.get('STAGING', False):
             os.system('git reset --hard HEAD; git clean -f -d; git pull')
             # TODO: Change the security permissions to make this less sketchy.
             os.system('supervisorctl restart clindesk-staging')
