24a8befb1de66b95392fad32ea02327afee179b3
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-08 17:20:18 -0500

    Updated with a special static handler.

--- clindesk.py
+++ clindesk.py
@@ -1,5 +1,8 @@
+import urlparse
 from flask import Flask, redirect, render_template, url_for, request
-app = Flask(__name__)
+app = Flask(import_name=__name__)
+
+app.config['STATIC_ROOT'] = 'http://static.clindesk.org/'
 
 # Set up logging in prod.
 # TODO: Check this actually works w/ ec2 mail & firewall, etc.
@@ -13,10 +16,26 @@ if not app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
+# Create a static() handler and send content to static.clindesk.org
+def static(path):
+    root = app.config.get('STATIC_ROOT', None)
+    if root is None: # fallback on the normal way
+        return url_for('static', filename=path)
+    return urlparse.urljoin(root, path)
 
+@app.context_processor
+def inject_static():
+    return dict(static=static)
+
+
+
+# The main URL settings go here
 @app.route("/")
 def welcome():
     return render_template('index.html')
+
+
+
 
 # Random-ish URL triggers a git pull for the staging deployment, only.
 @app.route("/github-pull-on-commit-M9tmMWz4XI", methods=['POST'])
@@ -33,4 +52,5 @@ def github_pull_on_commit():
 if __name__ == "__main__":
     # This is fine for prod purposes:
     #   The prod servers run via gunicorn & gevent, which won't invoke __main__
+    app.config['STATIC_ROOT'] = None
     app.run(host='0.0.0.0', port=5000, debug=True)
