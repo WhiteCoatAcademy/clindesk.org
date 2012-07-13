fb22c53bb8712d08cd240044b69b34bb747a949f
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-12 22:41:32 -0500

    Pattern WCA on ClinDesk for issue #4

--- wca.py
+++ wca.py
@@ -1,29 +1,61 @@
+import logging
 import os
+import re
 import urlparse
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, make_response, redirect, render_template, request, url_for
 app = Flask(import_name=__name__, static_folder='s')
 
-if os.environ.get('SUPERVISOR_PROCESS_NAME', False) == 'wca-staging':
-    app.config['STAGING'] = True
-else:
-    app.config['STATIC_ROOT'] = 'http://static.whitecoatacademy.org/s/'
 
-# Set up logging in prod.
-# TODO: Check this actually works w/ ec2 mail & firewall, etc.
-ADMINS = ['semenko+ec2crash@clindesk.org']
-if not app.debug:
-    import logging
+def register_email_logger(subject_tag, log_level):
+    """
+    This sends e-mails to ec2-crashes when something fails in Prod or Staging. 
+
+    It took me a while to figure this out:
+     ** The log hangler is basically *ignored* if debug=True **
+    """
+    ADMINS = ['ec2-prodlogs@clindesk.org']
+    from logging import Formatter
     from logging.handlers import SMTPHandler
-    mail_handler = SMTPHandler('aspmx.l.google.com',
-                               'ec2-crahes@clindesk.org',
-                               ADMINS, 'WCA Prod Failure')
-    mail_handler.setLevel(logging.ERROR)
+    mail_handler = SMTPHandler('email-smtp.us-east-1.amazonaws.com',
+                               'ec2-crashes@clindesk.org',
+                               ADMINS,
+                               'WCA %s Crash' % subject_tag,
+                               # Only sorta-secret. We haven't requested SES prod bits, so we can only
+                               # send to domains we own & addresses we verify. Little abuse potential. --semenko
+                               ('AKIAIEBTTF4MLQZ3CPAQ', 'AsD8aexgu9TUcIRB1bHmfG/zF2YMyv3Bze5LTpQzw6p1'),
+                               secure=())
+    mail_handler.setFormatter(Formatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+
+Message:
+
+%(message)s
+'''))
+    mail_handler.setLevel(log_level)
     app.logger.addHandler(mail_handler)
 
-# Create a static() handler and send content to static.whitecoatacademy.org
+    
+# Settings based on prod/staging/dev
+supervisor_name = os.environ.get('SUPERVISOR_PROCESS_NAME', False)
+if supervisor_name == 'wca-prod':
+    register_email_logger('Prod', logging.WARNING)
+    app.config['STATIC_ROOT'] = 'http://static.clindesk.org/s/'
+elif supervisor_name == 'wca-staging':
+    register_email_logger('Staging', logging.WARNING)
+    app.config['STAGING'] = True
+else:
+    # We're probably in a local dev instance.
+    pass
+
+
+# Create a static() handler and send content to static.clindesk.org
 def static(path):
     root = app.config.get('STATIC_ROOT', None)
-    if root is None: # fallback on the normal way
+    if root is None: # Just use /s/ instead of CDN
         return url_for('static', filename=path)
     return urlparse.urljoin(root, path)
 
@@ -32,28 +64,14 @@ def inject_static():
     return dict(static=static)
 
 
-
-# The main URL settings go here
 @app.route("/")
-def welcome():
-    return render_template('wca_index.html')
+@app.route("/index.html") # TODO: Standardize toplevel url.
+def page_index():
+    return render_template('teaser_wca.html')
 
-
-
-
-# Random-ish URL triggers a git pull for the staging deployment, only.
-@app.route("/github-pull-on-commit-M9tmMWz4XI", methods=['POST'])
-def github_pull_on_commit():
-    if request.environ['HTTP_X_REAL_IP'] in ('207.97.227.253', '50.57.128.197', '108.171.174.178'):
-        if app.config.get('STAGING', False):
-            os.system('git reset --hard HEAD; git clean -f -d; git pull')
-            # TODO: Change the security permissions to make this less sketchy.
-            os.system('supervisorctl restart wca-staging')
-            return "Pulling."
-    return "Access denied."
 
 if __name__ == "__main__":
     # This is fine for prod purposes:
     #   The prod servers run via gunicorn & gevent, which won't invoke __main__
-    app.config['STATIC_ROOT'] = None
     app.run(host='0.0.0.0', port=5000, debug=True)
+
