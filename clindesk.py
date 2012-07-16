ff0d58602f75e94088bc31a8c49a4b3538d6d823
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-16 18:28:50 -0500

    Disclaimer logic: 10-min TTL on non-prod. Change some values.

--- clindesk.py
+++ clindesk.py
@@ -147,10 +147,14 @@ def page_diseases_level2(level1, level2):
 @app.route("/setcookie", methods=['POST'])
 def clicked_disclaimer():
     """ Give the user a cookie if they dismiss the disclaimer. """
+    max_age = 60*60*24 # This is 24 hours
+    if app.config['STAGING'] or not app.config['ON_EC2']:
+        max_age = 60*10 # For staging, set a 10 minute TTL, so we don't forget the disclaimer.
+
     resp = make_response()
     resp.set_cookie(key='disclaimer',
-                    value='true',
-                    max_age=60*60*24, # This is 24 hours.
+                    value='MedEd_Should_Be_Open',
+                    max_age=max_age,
                     expires=None,
                     path='/',
                     domain=None,
