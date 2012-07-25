d96820859ab558aa589b4d0aa47622b7ec330f02
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-25 01:09:44 -0500

    Added minifier, and non-min sources, too!

---
+++ s/js/wca.js
@@ -0,0 +1,34 @@
+// Handle our disclaimer & get a cookie, yo.
+$(document).ready(function(){
+	if (document.cookie.match('disclaimer') != null) {
+	    $('#disclaimer').hide();
+	}
+	$('#ackbutton').click(function(){
+		$.ajax({
+			type: "POST",
+			    url: "/setcookie",
+			    success: function() {
+			    $('#disclaimer').hide();
+			}
+		    });
+	    });
+    });
+
+
+// Auto-tab highlighting.
+$(document).ready(function(){
+	var page = location.href.split(/\//)[3];
+	if(page) {
+	    if(page.indexOf('.') == -1) {
+		page += '/';
+	    }
+	} else {
+	    page = 'index.html';
+	}
+	var m = $("header ul.nav");
+	m.find('a[href$="'+page+'"]').parents("li").addClass("active");
+    });
+// TODO: Add that mobile tag from pixelentity.controller.js ?
+//  if mobile, $("html").addClass("mobile");
+
+// WCA-specific JS.
--- s/tools/minify-css-and-js.sh
+++ s/tools/minify-css-and-js.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-# 
-
+# Minify script to shrink CSS and JS.
 
 # Concatenate JS files into single files
-cat ../js/src/shared.js > ../js/min.cd.js
-cat ../js/src/shared.js > ../js/min.wca.js
-echo "" >> ../js/min.cd.js
-echo "" >> ../js/min.wca.js
-cat ../js/src/cd.js >> ../js/min.cd.js
-cat ../js/src/wca.js >> ../js/min.wca.js
+cat ../js/src/shared.js > ../js/cd.js
+cat ../js/src/shared.js > ../js/wca.js
+echo "" >> ../js/cd.js
+echo "" >> ../js/wca.js
+cat ../js/src/cd.js >> ../js/cd.js
+cat ../js/src/wca.js >> ../js/wca.js
 
-# yui-compressor --nomunge
+# Concatenate and compress the JS files.
+echo "ClinDesk JS"
+java -jar yuicompressor-2.4.7.jar --type js -v -o '../js/min.cd.js' ../js/cd.js
+echo ""
+
+echo "WCA JS"
+java -jar yuicompressor-2.4.7.jar --type js -v -o '../js/min.wca.js' ../js/wca.js
+
+
