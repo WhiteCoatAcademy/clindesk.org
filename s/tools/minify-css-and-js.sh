aa0e1c7f8add8e465438fea60ddf0fc59293b269
Nick Semenkovich <semenko@alum.mit.edu>
2012-07-26 22:50:37 -0500

    Minify CSS files, too!

--- s/tools/minify-css-and-js.sh
+++ s/tools/minify-css-and-js.sh
@@ -18,3 +18,27 @@ echo "WCA JS"
 java -jar yuicompressor-2.4.7.jar --type js -v -o '../js/min.wca.js' ../js/wca.js
 
 
+#### Let's do the same for CSS! 
+# Concatenate CSS into single files
+# (Do newlines matter?)
+cat ../css/bootstrap.css > ../css/_core.css
+echo "" >> ../css/_core.css
+cat ../css/bootstrap-responsive.css >> ../css/_core.css
+echo "" >> ../css/_core.css
+cat ../css/menu_layout.css >> ../css/_core.css
+echo "" >> ../css/_core.css
+cat ../css/menu_skin.css >> ../css/_core.css
+echo "" >> ../css/_core.css
+cat ../css/styles.css >> ../css/_core.css
+echo "" >> ../css/_core.css
+cat ../css/skin_blue.css >> ../css/_core.css
+
+cat ../css/_core.css > ../css/cd.css
+cat ../css/_core.css > ../css/wca.css
+
+echo "ClinDesk CSS"
+java -jar yuicompressor-2.4.7.jar --type css -v -o '../css/min.cd.css' ../css/cd.css
+
+echo "WCA CSS"
+java -jar yuicompressor-2.4.7.jar --type css -v -o '../css/min.wca.css' ../css/wca.css
+
