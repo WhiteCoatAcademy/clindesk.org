3082458230aa9ac01a76942ca50c8a4f4ed43970
Nick Semenkovich <semenko@alum.mit.edu>
2012-09-27 17:44:29 -0500

    Updated crushings.

--- s/crush-all-imgs.sh
+++ s/crush-all-imgs.sh
@@ -4,4 +4,7 @@
 
 find . -type f -name "*.png" -print0 | xargs -0 optipng -o7 
 
+# Via Google Chrome bugs & commits
+find . -type f -name "*.png" -print0 | xargs -0 pngcrush -reduce -brute -rem iTXt -rem tEXt -rem zTXt -rem tIME -ow 
+
 find . -type f -name "*.jpg" -print0 | xargs -0 jpegoptim -t --strip-all 
